820f5f870649653e1fb0e8cd44a3fd31a5705715
Richey Ward <richey@fartbox>
2020-01-19 23:23:32 +0000

    Fixed i3 load issue.

--- .profile
+++ .profile
@@ -7,3 +7,6 @@ for DOTFILE in $(find $profiledir); do
 		source "$DOTFILE"
 	fi
 done
+
+# Start graphical server if i3 not already running.
+[ "$(tty)" = "/dev/tty1" ] && ! pgrep -x i3 >/dev/null && exec startx
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-echo "Adding synlinks"
+echo "Adding symlinks"
 ln -svf $PWD/.profile ~
 ln -svf $PWD/.config ~
+
+echo "Installing Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
