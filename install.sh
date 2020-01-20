f204360a3b22b169adf65225483c928344698a45
pwnsauce <pwnsauce@fartbox>
2020-01-20 22:38:51 +0000

    Added .Xdefaults for urxvt

--- install.sh
+++ install.sh
@@ -1,7 +1,33 @@
-echo "Adding symlinks"
-ln -svf $PWD/.config ~
-ln -svf $PWD/.profile ~
-ln -svf $PWD/.local/bin ~/.local
+#!/bin/bash
 
-echo "Installing Vundle"
+cat << EOF
+  _____        _    __ _ _             _____           _        _ _           
+ |  __ \      | |  / _(_) |           |_   _|         | |      | | |          
+ | |  | | ___ | |_| |_ _| | ___  ___    | |  _ __  ___| |_ __ _| | | ___ _ __ 
+ | |  | |/ _ \| __|  _| | |/ _ \/ __|   | | | '_ \/ __| __/ _\` | | |/ _ \ '__|
+ | |__| | (_) | |_| | | | |  __/\__ \  _| |_| | | \__ \ || (_| | | |  __/ |   
+ |_____/ \___/ \__|_| |_|_|\___||___/ |_____|_| |_|___/\__\__,_|_|_|\___|_|"
+
+EOF
+
+echo "[*] - Adding symlinks"
+ln -svf $PWD/.config ~
+ln -svf $PWD/.local/bin ~/.local
+ln -svf $PWD/.profile ~
+ln -svf $PWD/.Xdefaults ~
+ln -svf $PWD/.Xdefaults ~/.Xresources
+echo
+
+
+
+
+echo "[*] - Installing packages"
+sudo apt -y install software-properties-common gpg
+sudo apt install -y $(cat packages.txt | tr '\n' ' ')
+
+
+echo "[*] - Installing Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo
+
+
